906d85be45b6df1ac9062449f848c037714a040e
zeia22 <122165265+zeia22@users.noreply.github.com>
2023-09-23 15:29:07 -0300

    sobre apis e express

--- build/index.js
+++ build/index.js
@@ -1,6 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const database_1 = require("./database");
+const database_2 = require("./database");
+console.log((0, database_1.createUser)("u003", "Astrodev", "astrodev@email.com", "astrodev99"));
+const busca = "Notebook";
+const nomeSearch = (0, database_2.searchProductsByName)(busca);
+console.log((0, database_1.getAllUsers)());
 console.table(database_1.users);
 console.table(database_1.products);
+console.log(nomeSearch);
 //# sourceMappingURL=index.js.map
